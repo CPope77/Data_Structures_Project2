cac36989fff5572ed97d47c21ffc99e3121601eb
CPope77 <connor@wirehedz.com>
2016-10-26 22:40:14 -0500

    Pushing everything into repository

---
+++ Project2/InfixToPostfix.java
@@ -0,0 +1,52 @@
+import java.util.*;
+import java.io.*;
+
+public class InfixToPostfix{
+		
+	public static Stack stack = new Stack();
+	
+	public static void main(String[] args)throws IOException{
+		String infix;
+		
+		System.out.println("\nEnter an algebraic infix: ");
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		infix = br.readLine();
+		
+		System.out.println("\nThe postfix of " + infix +" is: " + convertInfixToPostfix(infix));
+	}
+
+	private static String convertInfixToPostfix(String infix){
+		char symbol;
+		String postfix = "";
+		
+		for(int i = 0; i < infix.length(); i++){
+			symbol = infix.charAt(i);
+			if(Character.isLetter(symbol)){
+				postfix += symbol;
+			}
+			else if((symbol == ')') && (!stack.isEmpty())){
+				stack.pop();
+			}
+			else{
+				while(!stack.isEmpty() && !(stack.peek() == '(') && (evaluatePostfix(symbol) <= evaluatePostfix(stack.peek()))){
+					postfix += stack.pop();
+				}
+				stack.push(symbol);
+			}
+		}
+		while (!stack.isEmpty()){
+			postfix += stack.pop();
+		}
+		return postfix;
+	}
+		
+	private static int evaluatePostfix(char c){
+		if(c == '+' || c == '-'){
+			return 1;
+		}
+		if(c == '*' || c == '/' || c == '%' || c == '^'){
+			return 2;
+		}
+		return 0;
+	}
+}
---
+++ Project2/Stack.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.io.*;
+
+public class Stack{
+	private int arraySize;
+	private char[] charArray;
+
+	public Stack(){
+		charArray = new char[20];
+		arraySize = 0;
+	}
+	
+	public void push(char c) {
+		try{
+			charArray[arraySize] = c;
+			arraySize++;
+		}
+		catch(StackEmptyException e){
+			throw new StackEmptyException();
+		}
+	}
+	
+	public char pop() {
+		return charArray[arraySize--];
+	}
+	
+	public boolean isEmpty(){
+		if(arraySize == 0){
+			return true;
+		}
+		else{ return false; }
+	}
+	
+	public char peek(){
+		return charArray[arraySize];
+	}
+}

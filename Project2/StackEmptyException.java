b8f2de9788357c24a3bb7b55a0ad70a2a21a92ef
ZFCRacer <fowlercunningham@gmail.com>
2016-11-02 21:23:51 -0500

    Banner added

--- Project2/StackEmptyException.java
+++ Project2/StackEmptyException.java
@@ -1,3 +1,8 @@
+//*******************************************//
+//Created by Connor Pope & Zebulon Cunningham//
+//											 //
+//*******************************************//
+
 public class StackEmptyException extends RuntimeException {
 	public StackEmptyException() {
 		super("The stack is empty.");
